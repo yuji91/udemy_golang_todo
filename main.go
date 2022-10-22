2787e582c975286d6de99dd55b6679867bd4b248
yuji91 <yuji91.1227@gmail.com>
2022-10-22 21:13:25 +0900

    Create table users

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
   "fmt"
-  "./config" // import inner package
-  "log"
+  // "./config" // import inner package
+  //  "log"
+  "./app/models"
 )
 
 func main() {
-  fmt.Println(config.Config.Port)
-  fmt.Println(config.Config.SQLDriver)
-  fmt.Println(config.Config.DbName)
-  fmt.Println(config.Config.LogFile)
+  /*
+    fmt.Println(config.Config.Port)
+    fmt.Println(config.Config.SQLDriver)
+    fmt.Println(config.Config.DbName)
+    fmt.Println(config.Config.LogFile)
 
-  log.Println("test")
+    log.Println("test")
+  */
+
+  fmt.Println(models.Db)
 }
