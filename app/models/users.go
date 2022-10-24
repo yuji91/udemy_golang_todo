9647ad1892a2421f4e333d0e2740399664d125e0
yuji91 <yuji91.1227@gmail.com>
2022-10-24 22:53:18 +0900

    Add users.go

---
+++ app/models/users.go
@@ -0,0 +1,34 @@
+package models
+
+import "log"
+import "time"
+
+type User struct {
+  ID int
+  UUID string
+  Name string
+  Email string
+  PassWord string
+  CreatedAt time.Time
+}
+
+func (u *User) CreateUser() (err error) {
+  cmd := `INSERT INTO users(
+    uuid,
+    name,
+    email,
+    password,
+    created_at) values (?, ?, ?, ?, ?)` 
+
+  _, err = Db.Exec(cmd,
+      createUUID(),
+      u.Name, 
+      u.Email,
+      Encrypt(u.PassWord), 
+      time.Now())
+
+  if err != nil {
+    log.Fatalln(err)
+  }
+  return err
+}
--- main.go
+++ main.go
@@ -18,4 +18,12 @@ func main() {
   */
 
   fmt.Println(models.Db)
+
+  u := &models.User{}
+  u.Name = "test"
+  u.Email = "test@example.com"
+  u.PassWord = "testtest"
+  fmt.Println(u)
+
+  u.CreateUser()
 }
