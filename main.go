c81e6c5d7401bf0fa9d1765fc06de56f9f5aa672
yuji91 <yuji91.1227@gmail.com>
2022-10-25 00:17:07 +0900

    Add GetTodos

--- main.go
+++ main.go
@@ -49,8 +49,16 @@ func main() {
   /*
     user, _ := models.GetUser(2)
     user.CreateTodo("First Todo")
+
+    t, _ := models.GetTodo(1)
+    fmt.Println(t)
   */
 
-  t, _ := models.GetTodo(1)
-  fmt.Println(t)
+  user, _ := models.GetUser(2)
+  user.CreateTodo("Second Todo")
+  
+  todos, _ := models.GetTodos()
+  for _, v := range todos {
+    fmt.Println(v)
+  }
 }
